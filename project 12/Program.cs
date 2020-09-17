e6a6765a4d0c50254e2e5fd32b541787b6130647
tim <timmywhiting15@yahoo.com>
2020-09-17 18:18:42 -0400

    dawd

---
+++ project 12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace project_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type of your password");
+            
+            var isvalid = false;
+            while (!isvalid)
+            {
+                isvalid = makepassword();
+            }
+            Console.WriteLine("Password valid and accepted");
+         
+        }
+        public static bool makepassword()
+        {
+            var input = Console.ReadLine();
+            if (input.Length >= 7 &&
+                input.Length <= 12 &&
+                input.Contains("!") &&
+                input.Any(char.IsUpper) &&
+                input.Any(char.IsLower) &&
+                input.Any(char.IsLetterOrDigit))
+            {
+                return true;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Error");
+                return false;
+            }
+        }
+    }
+}
